058e18c61c98fd03e6af8b5b21630c99427786f0
Nhilowtech <baonhi050502@gmail.com>
2023-04-09 21:01:30 +0700

    Nhi

--- Group3_Project/Group3_Project/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Group3_Project.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
-
