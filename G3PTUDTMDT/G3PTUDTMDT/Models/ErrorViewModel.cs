4f3a752adad71d2af85ab0f13d24b4a71b80a6b2
Nhilowtech <baonhi050502@gmail.com>
2023-04-11 14:55:31 +0700

    Nhi

---
+++ G3PTUDTMDT/G3PTUDTMDT/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace G3PTUDTMDT.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
